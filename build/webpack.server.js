8950f6dc2c4c24b351e6cedd26b296db33017eff
雁北去 <478760274@qq.com>
2019-03-15 17:25:55 +0800

    添加webpack配置文件说明

--- build/webpack.server.js
+++ build/webpack.server.js
@@ -16,17 +16,20 @@ const clientConfig = {
         rules: [{
             test: /\.(css|styl)$/,
             use: [
+                //webpack 是从下向上执行 所以是 先解析stylus 
+                //然后添加 浏览器兼容postcss 在 CSS文件引入到对应的入口文件里(css-loader)
+                //最后创造一个style标签，将引入的css放置进去(style-loader or isomorphic-style-loader)
                 'isomorphic-style-loader', {
                     loader: 'css-loader',
                     options: {
                         importLoaders: 1,
                         modules: true,
                         localIdentName: '[name]_[local]_[hash:base64:5]'
-                    },                  
-                }, 'postcss-loader','stylus-loader'
+                    },
+                }, 'postcss-loader', 'stylus-loader'
             ]
         },]
-    }
+    },
 }
 //
 module.exports = merge(clientConfig, _config);
