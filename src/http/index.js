7c18745971db8daa0614d4e116b8c230d641ecad
雁北去 <478760274@qq.com>
2019-03-15 17:26:16 +0800

    优化环境判断的惰性函数

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,12 @@
 import clientHttp from '../http/client'
 import serverHttp from '../http/server'
-const http = (function () {
-    let http = null
-    return function(){
+let http = function () {
+    const _http = typeof window === 'undefined' ? serverHttp : clientHttp
+    http = function () {
         //好处在于 如果不使用http()  不用执行判断  使用了一次之后也不用多次判断 所谓惰性
-        return http || (http = typeof window === 'undefined' ? serverHttp : clientHttp)
+        return _http
     }
-})()
+    return http()
+}
 //用于请求数据 只负责请求接口
 export default http
